796c9085d0447da9e0eb9fab0748609d5920f5ac
David Enoma <davidenoma@gmail.com>
2023-03-05 12:55:54 -0700

    modelling giving improved mse and r-squared

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import numpy as np
 import pandas as pd
 from sklearn import linear_model, svm
 from sklearn.linear_model import LinearRegression, Ridge
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.neural_network import MLPRegressor
 from xgboost import XGBRegressor
 
-ospath = "/home/davidenoma/Documents/MDGE612"
+ospath = "C:/Users/HP/OneDrive/Desktop/PhD._BMB/COURSE_WORK/MDGE612"
+#/home/davidenoma/Documents/MDGE612
 
 genotype_path = ospath+"/call_method_54.tair9.FT10.csv"
 phenotype_path = ospath+"/FT10.txt"
@@ -16,16 +18,15 @@ phenotype_path = ospath+"/FT10.txt"
 #File specification at: https://zzz.bwh.harvard.edu/plink/data.shtml
 def genotype_cleaning():
     genotype = pd.read_csv(genotype_path)
-    # snpids = []
-    # for i in range(genotype.shape[0]):     #     snpids.append("SNP" + str(i))
+    snpids = []
+    for i in range(genotype.shape[0]):
+        snpids.append("SNP" + str(i))
+    genotype.insert(1,"snpid",snpids)
 
     gen_distance = [0] * genotype.shape[0]
-    genotype.insert(2,"gen distance",gen_distance)
-    genotype.to_csv("genotypo.tped")
-    chrom_snp_id_gen_distance_bp = genotype.iloc[:,:4]
-    # print(genotype.head())
-    # final_genotype = pd.concat([chrom_snp_id_gen_distance_bp,genotype.iloc[1:,1:]],axis=1)
-    # print(final_genotype.head(),final_genotype.shape)
+    genotype.insert(2,"gen_distance",gen_distance)
+
+    genotype.to_csv(ospath+'/new_genotype.tped',sep=" ",index=False)
 
 
 
@@ -41,73 +42,58 @@ def phenotype_cleaning():
     phenotype.insert(0, 'paternal_id', paternal_id)
     phenotype.insert(0, 'invidual_id', individual_id)
     phenotype.insert(0, 'family_id', family_id)
-    phenotype.to_csv(ospath+'/phenotype_recode.csv',index=False,sep=" ")
 
-# #Create a new tped file with duplicated alleles at each position
-# #Multiply all alleles at each postion
-def mulitply_alleles(example_ped):
-    for i in range(example_ped.shape[0]):
-        x = example_ped.iloc[i, 4:]
-        new_list = list()
-        [new_list.extend([z] * 2) for z in list(x)]
-        if i == 0:
-            new_example_ped = pd.DataFrame(new_list)
-            new_example_ped = new_example_ped.transpose()
-        else:
-            update_ped = pd.DataFrame(new_list)
-            update_ped = update_ped.transpose()
-            new_example_ped = new_example_ped.append(update_ped, ignore_index=True)
-    print(example_ped.iloc[:, :4].shape, new_example_ped.shape)
-    combined_final_tped = pd.merge(example_ped.iloc[:, :4], new_example_ped, left_index=True, right_index=True)
-    combined_final_tped.columns = range(combined_final_tped.shape[1])
-    print(combined_final_tped.shape)
-    combined_final_tped.to_csv(ospath + '/new_trans.tped')
+    phenotype.to_csv(ospath+'/new_phenotype.tfam',index=False,sep=" ")
 
 def saveToDisk(genotype,phenotype):
-    genotype.to_csv(ospath + '/genotype.csv', index=False)
-    phenotype.to_csv(ospath + '/phenotype.csv', index=False, sep=" ")
+    genotype.to_csv(ospath+'/new_genotype.tped', sep=" ",index=False)
+    phenotype.to_csv(ospath+'/new_phenotype.tfam', index=False, sep=" ")
 
 def QConPreditionFiles(genotype,phenotype):
-    genotype = pd.read_csv(genotype)
+    genotype = pd.read_csv(genotype,sep=" ",index_col=None)
     phenotype = pd.read_csv(phenotype, sep=" ")
     phenotype = phenotype.dropna()
 
-    true_pheno = phenotype.iloc[:, 0].values
+    true_pheno = phenotype.loc[:, 'ecotype_id'].values
     index = [str(x) for x in true_pheno]
-    i = 2
+    i = 4
+    print(genotype.shape,phenotype.shape)
 
     while i < len(genotype.columns):
         if genotype.columns[i] not in index:
+            print('found genotype missing')
             genotype.drop(columns=genotype.columns[i], inplace=True)
         i = i + 1
+
+    i = 0
     while i < len(index):
         if index[i] not in genotype.columns:
-            index.remove(index[i])
+            phenotype=phenotype[phenotype.ecotype_id != true_pheno[i]]
+            print('found pheno missing ')
         i = i + 1
-
+    print(genotype.shape, phenotype.shape)
     return genotype,phenotype
 
 
 def modelling():
     # Split the data into training and testing sets
 
-    genotype = pd.read_csv(ospath + '/genotype2num', index_col=None)
+    genotype = pd.read_csv(ospath + '/updated_recode_file', index_col=None)
     genotype = genotype.transpose()
-    phenotype = pd.read_csv(ospath + '/phenotype.csv', index_col=None, sep="\t")
+    phenotype = pd.read_csv(ospath + '/phenotype_for_jawa', index_col=None, sep="\t")
     genotype.set_axis([str(x) for x in genotype.iloc[1, :].values], axis="columns", inplace=True)
     # genotype.columms = genotype.iloc[1,:].values
     genotype.drop(['Chromosome', 'Positions'], inplace=True)
-
     # read in coordinates after genotype mapping
-    emmax = pd.read_csv(ospath + '/emmax_stepwise_clean/EMMAX.0_5_FT10.top.csv')
+    emmax = pd.read_csv(ospath + '/emmax_step_wise_manual_recode/EMMAX.0_5_FT10.top.csv')
 
     # Selecting the top 100 Features affter sorting in ascending order based on p-value
-    emmax_coord = [str(x) for x in emmax.iloc[:, 1].values]
+    ##The sorting was done on excel.
+    emmax_coord = [str(x) for x in emmax.iloc[:19, 1].values]
 
-    # lm = pd.read_csv(ospath+'/lm_clean/LM.0_5_FT10.top.csv')
-    # lm_coord = [str(x) for x in lm.iloc[:100,1].values]
-    print(genotype.shape)
-    print(emmax_coord)
+    lm = pd.read_csv(ospath+'/lm_manual_recode/LM.0_5_FT10.top.csv')
+    lm_coord = [str(x) for x in lm.iloc[:49,1].values]
+
     # Split into train and test set
     X_train, X_test, y_train, y_test = train_test_split(genotype.loc[:, emmax_coord], phenotype.iloc[:, 1].values,
                                                         test_size=0.2, random_state=0)
@@ -120,7 +106,10 @@ def modelling():
     mse = mean_squared_error(y_test, y_pred)
     # Print the mean squared error
     print("Mean Squared Error: Linear Regression", mse)
-
+    # Calculate R-squared
+    r_squared = r2_score(y_test, y_pred)
+    print("R-squared for : Linear Regression", r_squared)
+    print("\n")
     # Ridge regression
 
     ridge_model = Ridge(alpha=1.0).fit(X_train, y_train)
@@ -130,6 +119,10 @@ def modelling():
     mse = mean_squared_error(y_test, y_pred)
     # Print the mean squared error
     print("Mean Squared Error Ridge Regression :L2 ", mse)
+    # Calculate R-squared
+    r_squared = r2_score(y_test, y_pred)
+    print("R-squared for Ridge Regression L2: ", r_squared)
+    print("\n")
 
     lasso_model = linear_model.Lasso(alpha=0.1).fit(X_train, y_train)
     # Predict the phenotype using the genotypes
@@ -138,6 +131,10 @@ def modelling():
     mse = mean_squared_error(y_test, y_pred)
     # Print the mean squared error
     print("Mean Squared Error Lasso L1 Regression:", mse)
+    # Calculate R-squared
+    r_squared = r2_score(y_test, y_pred)
+    print("R-squared for Lasso L1 Regression:", r_squared)
+    print("\n")
 
     # SVR
     svm_model = svm.SVR(epsilon=0.4).fit(X_train, y_train)
@@ -146,25 +143,50 @@ def modelling():
     # Calculate the mean squared error
     mse = mean_squared_error(y_test, y_pred)
     print("Mean Squared Error(SVR):", mse)
+    # Calculate R-squared
+    r_squared = r2_score(y_test, y_pred)
+    print("R-squared for SVR:", r_squared)
+    print("\n")
 
-    #
-    X_train = X_train.to_numpy()
-    X_test = X_test.to_numpy()
-    print(X_test.shape, X_train.shape)
 
-    model = XGBRegressor().fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
+
+
+    #For complex models, it might be useful to have more features
+    # Split into train and test set
+    emmax_coord = [str(x) for x in emmax.iloc[:, 1].values]
+    lm_coord = [str(x) for x in lm.iloc[:1999, 1].values]
+    X_train, X_test, y_train, y_test = train_test_split(genotype.loc[:, emmax_coord], phenotype.iloc[:, 1].values,
+                                                        test_size=0.2, random_state=0)
+
+    model = XGBRegressor().fit(X_train.to_numpy(), y_train)
+    y_pred = model.predict(X_test.to_numpy())
+    mse = mean_squared_error(y_test,y_pred)
     print("Mean Squared Error (XGBOOST):", mse)
+    # Calculate R-squared
+    r_squared = r2_score(y_test, y_pred)
+    print("R-squared for XGBOOST:", r_squared)
+    print("\n")
+
+    # For complex models, it might be useful to have more features
+    # Split into train and test set
+    emmax_coord = [str(x) for x in emmax.iloc[:, 1].values]
+    lm_coord = [str(x) for x in lm.iloc[:1999, 1].values]
+    X_train, X_test, y_train, y_test = train_test_split(genotype.loc[:, lm_coord], phenotype.iloc[:, 1].values,
+                                                        test_size=0.2, random_state=0)
+
+    # hidden_layer_sizes = (50, 10, 2)
+    model = MLPRegressor(hidden_layer_sizes = (100,50, 30, 2),random_state=1, activation="relu", solver="adam", max_iter=1000)
+    # Train model
+    model.fit(X_train, y_train)
+
+    # Make predictions on test data
+    y_pred = model.predict(X_test)
+    # Calculate R-squared
+    r_squared = r2_score(y_test, y_pred)
+    print("R-squared for Neural network:", r_squared)
+    mse = mean_squared_error(y_test, y_pred)
+    print("Mean Squared Error ( Neural network:):", mse)
+    print("\n")
 
 #Implementation line
-genotype_cleaning()
-
-phenotype_cleaning()
-genotype_clean, phenotype_clean = QConPreditionFiles(genotype=genotype_path,phenotype=ospath+'/phenotype_recode.csv')
-print(genotype_clean.head(),phenotype_clean.head())
-print(genotype_clean.shape,phenotype_clean.shape)
-
-
-
-
+modelling()
